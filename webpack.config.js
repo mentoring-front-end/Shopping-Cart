e9d134d1e2405a88fd6c245aeaaab48af90a205c
Adam Abdillah <adamabdillah20@gmail.com>
2021-12-31 16:53:04 +0700

    fix: setup builder config: using typescript and tailwindcss from scratch

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,35 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, './src/index.js'),
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test:/\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader"
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack', 'url-loader'],
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "index.css",
-    })
-  ]
-};
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test:/\.css$/,
+				use:[
+				MiniCssExtractPlugin.loader,
+				"css-loader",
+				"postcss-loader"
+				]
+			}
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],		
+	},
+	output: {
+		filename: 'webpack-bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [new MiniCssExtractPlugin({
+		filename:"index.css",
+	})],
+	devServer: {
+		static: path.resolve(__dirname, 'dist')
+	}
+}
